6ba175b9b4c1e93bd474c1a279a9e6d81a5f8702
work <reyna0304@gmail.com>
2020-03-31 15:47:31 +0800

    restful get csv

---
+++ api.py
@@ -0,0 +1,105 @@
+import flask
+from flask import jsonify, request
+import numpy as np
+import pandas as pd
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+app.config["JSON_AS_ASCII"] = False
+
+# test data
+tpe = {
+    "id": 0,
+    "city_name": "台北",
+    "country_name": "台灣",
+    "is_capital": True,
+    "location": {
+        "longitude": 121.569649,
+        "latitude": 25.036786
+    }
+}
+nyc = {
+    "id": 1,
+    "city_name": "紐約",
+    "country_name": "美國",
+    "is_capital": False,
+    "location": {
+        "longitude": -74.004364,
+        "latitude": 40.710405
+    }
+}
+ldn = {
+    "id": 2,
+    "city_name": "倫敦",
+    "country_name": "英國",
+    "is_capital": True,
+    "location": {
+        "longitude": -0.114089,
+        "latitude": 51.507497
+    }
+}
+cities = [tpe, nyc, ldn]
+
+gapminder = pd.read_csv("gapminder.csv")
+gapminder_list = []
+nrows = gapminder.shape[0]
+for i in range(nrows):
+    ser = gapminder.loc[i, :]
+    row_dict = {}
+    for idx, val in zip(ser.index, ser.values):
+        if type(val) is str:
+            row_dict[idx] = val
+        elif type(val) is np.int64:
+            row_dict[idx] = int(val)
+        elif type(val) is np.float64:
+            row_dict[idx] = float(val)
+    gapminder_list.append(row_dict)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Hello Flask!</h1>"
+
+
+@app.route('/cities/all', methods=['GET'])
+def cities_all():
+    return jsonify(cities)
+
+
+@app.route('/cities', methods=['GET'])
+def city_name():
+    if 'city_name' in request.args:
+        city_name = request.args['city_name']
+    else:
+        return "Error: No city_name provided. Please specify a city_name."
+    results = []
+
+    for city in cities:
+        if city['city_name'] == city_name:
+            results.append(city)
+
+    return jsonify(results)
+
+
+@app.route('/gapminder/all', methods=['GET'])
+def gapminder_all():
+    return jsonify(gapminder_list)
+
+
+@app.route('/gapminder', methods=['GET'])
+def country():
+    if 'country' in request.args:
+        country = request.args['country']
+    else:
+        return "Error: No country provided. Please specify a country."
+    results = []
+
+    for elem in gapminder_list:
+        if elem['country'] == country:
+            results.append(elem)
+
+    return jsonify(results)
+
+
+app.run()
